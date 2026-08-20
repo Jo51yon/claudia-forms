3fa27ed8d72c9779fd852182303afd9975f7fd46
Jo51yon <jolyon.grace@gracenet.co.uk>
2026-08-20 20:32:16 +0100

    initial: src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { default as ClaudiaFormRenderer } from './ClaudiaFormRenderer';
+export type { ClaudiaFormRendererProps } from './ClaudiaFormRenderer';
+export { DEFAULT_FIELD_REGISTRY } from './defaultFieldRegistry';
+export type {
+  ClaudiaFormField, ClaudiaFieldValue, ClaudiaFieldRendererProps,
+  ClaudiaFieldRenderer, ClaudiaFieldRegistry,
+} from './types';
